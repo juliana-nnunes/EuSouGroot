a3c1076c4385c9caa8bd233b71240c3ecb514ecf
juliana-nnunes <josecarloslimanunes30@gmail.com>
2025-11-28 20:08:43 -0300

    atividade conccluída

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+            static string pergunta = "";
+            static string resposta = "";
+            static string saída = "tchau";
+
+
+            static void Main ()
+
+    {
+        Console.WriteLine("--- Groot Chatboot ---\n");
+
+
+        while (true)
+        {
+            PerguntaUsuario();
+
+            if (pergunta == saída)
+
+            {
+                 
+                 resposta = "Eu sou o Groot!";
+                 Console.WriteLine($"\nResposta: {resposta}");
+                 break;
+
+            }
+
+            resposta = "Eu sou o Groot.";
+            Console.WriteLine($"\nResposta: {resposta}\n");
+
+        }
+    }
+
+    static void PerguntaUsuario()
+
+    {
+        Console.Write("Pergunta: ");
+        pergunta = Console.ReadLine()!.ToLower().Trim();
+    }
+}
